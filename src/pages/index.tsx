ef2e2cc3f802f172e4e1f5230b1412237ff08345
caiohenrique-developer <caiorussellrox@msn.com>
2021-04-20 18:19:39 -0300

    :sparkles: [ FEAT ]: Learn about getStaticProps getServerSideProps to get server side datas on 1st render page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>Index</h1>;
+export default function Home({ episodes }) {
+  return (
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </>
+  );
+}
+
+// The data obtained from the server automatically passes to my index component above
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
